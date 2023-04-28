d6e52f9627249824c40c4ae8ba962c3170d20a89
Gerald-Ikenwaike <ikenwaikegerald@gmail.com>
2023-04-27 18:44:39 -0700

    updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,14 +16,18 @@ list_t *add_node(list_t **head, const char *str)
 	unsigned int len = 0;
 
 	while (str[len])
+	{
 		len++;
-		new = malloc(sizeof(list_t));
+	}
+	new = malloc(sizeof(list_t));
 	if (!new)
+	{
 		return (NULL);
-		new->str = strdup(str);
-		new->len = len;
-		new->next = (*head);
-		(*head) = new;
+	}
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
 
 	return (*head);
 }
